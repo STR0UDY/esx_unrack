6ad1351159d0dc9ceec894800134c1c841f37f27
Stroudy <32533859+STR0UDY@users.noreply.github.com>
2019-05-31 13:20:24 +0100

    Upload

---
+++ client/main.lua
@@ -0,0 +1,94 @@
+local PlayerData		= {}
+ESX							   = nil
+
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+    PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+	PlayerData.job = job
+end)
+
+RegisterCommand("unrack", function(source, args, rawCommand)
+	local ped = PlayerPedId()
+	local closestVehicle, Distance = ESX.Game.GetClosestVehicle()
+	local vehicleCoords = GetEntityCoords(closestVehicle)
+	local weapon =	GetHashKey("WEAPON_CARBINERIFLE")
+	local class = GetVehicleClass(closestVehicle)
+
+	if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+	if Distance < 2.0  and class == 18 and not IsPedInAnyVehicle(ped, false) then
+		TriggerEvent("mythic_progbar:client:progress", {
+			name = "action_rifle_unrack",
+			duration = 4000,
+			label = "Unracking Rifle...",
+			useWhileDead = false,
+			canCancel = false,
+			controlDisables = {
+				disableMovement = false,
+				disableCarMovement = true,
+				disableMouse = false,
+				disableCombat = true,
+			},
+			animation = {
+				animDict = "missheistdockssetup1clipboard@idle_a",
+				anim = "idle_a",
+			},
+			prop = {
+				model = "prop_paper_bag_small",
+			}
+		}, function(status)
+			if not status then
+				GiveWeaponToPed(ped, weapon, 130, false, true)
+			end
+		end)
+	end
+end
+end, false)
+
+RegisterCommand("rack", function(source, args, rawCommand)
+	local ped = PlayerPedId()
+	local closestVehicle, Distance = ESX.Game.GetClosestVehicle()
+	local vehicleCoords = GetEntityCoords(closestVehicle)
+	local weapon =	GetHashKey("WEAPON_CARBINERIFLE")
+	local class = GetVehicleClass(closestVehicle)
+
+	if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+	if Distance < 2.0  and class == 18 and not IsPedInAnyVehicle(ped, false) then
+		TriggerEvent("mythic_progbar:client:progress", {
+			name = "action_rack_rifle",
+			duration = 4000,
+			label = "Racking Rifle",
+			useWhileDead = false,
+			canCancel = true,
+			controlDisables = {
+				disableMovement = false,
+				disableCarMovement = true,
+				disableMouse = false,
+				disableCombat = true,
+			},
+			animation = {
+				animDict = "missheistdockssetup1clipboard@idle_a",
+				anim = "idle_a",
+			},
+			prop = {
+				model = "prop_paper_bag_small",
+			}
+		}, function(status)
+			if not status then
+				RemoveWeaponFromPed(ped, weapon)
+			end
+		end)
+	end
+end
+end, false)
